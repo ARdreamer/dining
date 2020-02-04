4e948d3c660d3d0ceb60bdd9a4bb61237529fee8
baojx <18846078661@163.com>
2020-02-04 14:34:04 +0800

    first commit

---
+++ src/main/java/com/order/dining/DiningApplication.java
@@ -0,0 +1,17 @@
+package com.order.dining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: baojx
+ * @Date: 2020/1/29 19:41
+ */
+@SpringBootApplication
+public class DiningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiningApplication.class, args);
+    }
+
+}
