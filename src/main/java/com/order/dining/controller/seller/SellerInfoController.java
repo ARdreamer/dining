d45bc33c105d6ad20dc14a0f8d6beaaebb3b6696
baojx <18846078661@163.com>
2020-02-24 13:27:00 +0800

    卖家信息

---
+++ src/main/java/com/order/dining/controller/seller/SellerInfoController.java
@@ -0,0 +1,10 @@
+package com.order.dining.controller.seller;
+
+/**
+ * @Author: baojx
+ * @Date: 2020/2/24 13:25
+ * @Desc: 卖家信息
+ */
+public class SellerInfoController {
+    //todo 增加用户信息登陆 登出 注册功能
+}
