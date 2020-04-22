e0c9703789f7c95b7161c5d6a3c52cee3df31a0e
drama <18846078661@163.com>
2020-04-22 23:06:16 +0800

    修改格式验证

--- src/main/java/com/order/dining/beans/form/CategoryForm.java
+++ src/main/java/com/order/dining/beans/form/CategoryForm.java
@@ -2,6 +2,10 @@ package com.order.dining.beans.form;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 /**
  * @Author: baojx
  * @Date: 2020/2/21 14:30
@@ -12,8 +16,11 @@ public class CategoryForm {
 
     private Integer categoryId;
 
+    @NotEmpty(message = "商品类目名称必填")
     private String categoryName;
 
+    @NotNull(message = "商品类目no必填")
+    @Min(message = "商品类目no不能小于0", value = 0)
     private Integer categoryNo;
 
 }
